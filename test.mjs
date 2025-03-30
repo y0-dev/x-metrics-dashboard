a466c75719835629bb9cbbb6e6b16e91d82fd091
git <contact@yoyo.dev>
2025-03-30 23:17:31 +0200

    Add OF selenium test

--- test.mjs
+++ test.mjs
@@ -21,6 +21,7 @@ describe('search', async function () {
         await driver.wait(until.elementLocated(By.id('input-14')));
         await delay(3000);
         await driver.wait(until.elementIsVisible(driver.findElement(By.id('input-14'))));
+        await driver.wait(until.elementIsEnabled(driver.findElement(By.id('input-14'))));
 
         //const source = await driver.getPageSource();
         //if (source.includes('you are human'))
