f9ecff7e93f7e1e8fc9c76c09451346fca886716
git <contact@yoyo.dev>
2025-05-14 12:31:40 +0700

    fix reddit fetch post count

--- reddit_fetch_data.js
+++ reddit_fetch_data.js
@@ -28,7 +28,11 @@ const getToken = async () => {
 };
 
 const fetchRedditPostsCount = async () => {
-	const response = await fetch('https://www.reddit.com/user/'+REDDIT_USERNAME+'/submitted/new.json');
+	const response = await fetch('https://www.reddit.com/user/'+REDDIT_USERNAME+'/submitted/new.json', {
+		headers: {
+			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
+		},
+	});
 
 	if (!response.ok) {
 		throw new Error(`HTTP error! status: ${response.status} for ${response.url}`);
