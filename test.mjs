a699e0a6431eddc6864d8eb3e42e81a3ccdb7ff8
git <contact@yoyo.dev>
2025-08-03 21:32:00 +0200

    Test

--- test.mjs
+++ test.mjs
@@ -17,38 +17,23 @@ describe('search', async function () {
     const search = async () => {
         // Automate DuckDuckGo search
         const bridge = new FlutterSeleniumBridge(driver);
-        await driver.get('https://onlyfans.com/');
-        await bridge.enableAccessibility();
+        await driver.get('https://onlyfans.com/'+process.env.USERNAME);
+        //await bridge.enableAccessibility();//Attempt to click on "EnableAccessibility" button failed. Retrying... => je pense qu'OF on désactivé ce mode de dev
 
         // Wait until the result page is loaded
-        await driver.wait(until.elementLocated(By.id('input-14')));
+        await driver.wait(until.elementLocated(By.css('*[data-icon-name="icon-back"]')));
         //await delay(2000);
-        await driver.wait(until.elementIsVisible(driver.findElement(By.id('input-14'))));
-        await driver.wait(until.elementIsEnabled(driver.findElement(By.id('input-14'))));
+        await driver.wait(until.elementIsVisible(driver.findElement(By.css('*[data-icon-name="icon-back"]'))));
+        await driver.wait(until.elementIsEnabled(driver.findElement(By.css('*[data-icon-name="icon-back"]'))));
 
         //const source = await driver.getPageSource();
         //if (source.includes('you are human'))
         //    throw new Error('Cloudflare: You are not human');
         //else throw new Error(source);
 
-        const emailBox = await driver.findElement(By.id('input-14'));
-        const passwordBox = await driver.findElement(By.id('input-17'));
+        var FanCountN = 0;
+
         try {
-            //await emailBox.sendKeys(process.env.EMAIL);//TODO ElementNotInteractableError: element not interactable
-            //await passwordBox.sendKeys(process.env.PASSWORD, Key.ENTER);
-            //https://www.reddit.com/r/flutterhelp/comments/187dqfe/how_enter_input_field_values_using_javascript/ => impossible avec flutter
-            //driver.executeScript("arguments[0].select();arguments[0].value='"+process.env.EMAIL+"';", emailBox);
-            //driver.executeScript("arguments[0].select();arguments[0].value='"+process.env.PASSWORD+"';", passwordBox);
-            const emailInput = await bridge.activateInputField(By.id('input-14'));
-            await emailInput.sendKeys(process.env.EMAIL);
-            const passwordInput = await bridge.activateInputField(By.id('input-17'));
-            await passwordInput.sendKeys(process.env.PASSWORD, Key.ENTER);
-            console.log("entering credentials");
-
-            //TODO there is actually no input value and button is still gray lol
-            driver.findElement(By.css('.g-btn.m-rounded.m-block.m-md.mb-0')).click();
-            console.log("logging in");
-
             const filename = "test"
                 .replace(/['"]+/g, '')
                 .replace(/[^a-z0-9]/gi, '_')
@@ -56,18 +41,11 @@ describe('search', async function () {
             const encodedString = await driver.takeScreenshot();
             await fs.writeFileSync(`./screenshots/${filename}.png`, encodedString, 'base64');
 
-            // Wait until the result page is loaded
-            await driver.wait(until.elementLocated(By.xpath('//*[@data-name="Notifications"]')));
-            console.log("logged in");await delay(3000);
-
-            const Avatar = await driver.findElement(By.css('.g-avatar__img-wrapper'));
-            Avatar.click();await delay(2000);console.log("getting profile");
-
-            await driver.wait(until.elementLocated(By.xpath('(//span[@class="l-sidebar__user-data__item__count"])[1]')));
+            //await driver.wait(until.elementLocated(By.xpath('(//span[@class="b-profile__sections__count g-semibold"])[1]')));
+            //console.log("getting fan count");
+            const FanCount = await driver.findElement(By.xpath('(//span[@class="b-profile__sections__count g-semibold"])[1]'));
             console.log("getting fan count");
-            const FanCount = await driver.findElement(By.xpath('(//span[@class="l-sidebar__user-data__item__count"])[1]'));
-            console.log("getting fan count");
-            const FanCountN = FanCount.text();
+            FanCountN = FanCount.text();
         } catch (e) {
             const filename = "test"
                 .replace(/['"]+/g, '')
