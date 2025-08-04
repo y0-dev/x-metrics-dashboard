f34e3eacb9502109f62d8bcc3d0b6c929cbf9471
git <contact@yoyo.dev>
2025-08-04 13:44:53 +0200

    Add ig

--- ig_fetch_data.js
+++ ig_fetch_data.js
@@ -11,14 +11,17 @@ const fetchRedditFollowerCount = async () => {
   }
 
   const html = await response.text();
-  const des = html.match(/<meta property="og:description" content="(.*?)"/)[1].replace(/&[#A-Za-z0-9]+;/gi, '');
-  const numbers = des.match(/\d+/g);
+  var des = html.match(/<meta property="og:description" content="(.*?)"/);
+  if(des){
+    des = [1].replace(/&[#A-Za-z0-9]+;/gi, '');
+    const numbers = des.match(/\d+/g);
 
-  // Extract the metrics
-  const metrics = {followers_count: parseInt(numbers[0]), following_count: parseInt(numbers[1]), posts_count: parseInt(numbers[2])};
+    // Extract the metrics
+    const metrics = {followers_count: parseInt(numbers[0]), following_count: parseInt(numbers[1]), posts_count: parseInt(numbers[2])};
 
-  // Write the metrics to the environment file
-  fs.appendFileSync(process.env.GITHUB_OUTPUT, `METRICS=${JSON.stringify(metrics)}\n`);
+    // Write the metrics to the environment file
+    fs.appendFileSync(process.env.GITHUB_OUTPUT, `METRICS=${JSON.stringify(metrics)}\n`);
+  } else console.log(html);
 };
 
 fetchRedditFollowerCount().catch(err => console.error(err));
