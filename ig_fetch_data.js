6cac821333e058ca8fad83136b280674a6970b22
git <contact@yoyo.dev>
2026-02-19 20:44:26 +0100

    Fix IG fetch

--- ig_fetch_data.js
+++ ig_fetch_data.js
@@ -2,19 +2,18 @@ require('dotenv').config();
 const fs = require('fs');
 const randomUseragent = require('random-useragent');
 
-const fetchRedditFollowerCount = async () => {
+const fetchIGFollowerCount = async () => {
   const response = await fetch('https://i.instagram.com/api/v1/users/web_profile_info/?username='+process.env.USERNAME, {
     headers: {
       'Accept': '*/*',
       'Accept-Encoding': 'gzip, deflate, br',
+      'Accept-Language': 'en-US,en;q=0.9',
       'Connection': 'keep-alive',
       'User-Agent': "Instagram 76.0.0.15.395 Android (24/7.0; 640dpi; 1440x2560; samsung; SM-G930F; herolte; samsungexynos8890; en_US; 138226743)",
       'Origin': 'https://www.instagram.com',
       'Referer': 'https://www.instagram.com',
-    },
-    cookies: {
-        "sessionid": "value",
-        "csrftoken": "value"
+      "X-IG-App-ID": "936619743392459",
+      'Cookies': 'sessionid=value; csrftoken=value;',
     }
   });
 
@@ -35,4 +34,4 @@ const fetchRedditFollowerCount = async () => {
     fs.appendFileSync(process.env.GITHUB_OUTPUT, `METRICS=${JSON.stringify(metrics)}\n`);
 };
 
-fetchRedditFollowerCount().catch(err => console.error(err));
+fetchIGFollowerCount().catch(err => console.error(err));
