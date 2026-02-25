e4cc16ea7cc728fb01140c5c160ba02949bcca67
git <contact@yoyo.dev>
2026-02-25 17:36:37 +0100

    Fix IG fetch

--- ig_fetch_data.js
+++ ig_fetch_data.js
@@ -4,7 +4,8 @@ const randomUseragent = require('random-useragent');
 
 const fetchIGFollowerCount = async () => {
   const response = await fetch(
-      'https://instagram120.p.rapidapi.com/api/instagram/profile'
+      'https://instagram-profile1.p.rapidapi.com/getprofile/'+process.env.USERNAME
+      //'https://instagram120.p.rapidapi.com/api/instagram/profile'//based on public logged out data so fake followers number, same as instagram-looter2
       //'https://i.instagram.com/api/v1/users/web_profile_info/?username='+process.env.USERNAME
       , {
     // headers: {
@@ -18,10 +19,10 @@ const fetchIGFollowerCount = async () => {
     //   "X-IG-App-ID": "936619743392459",
     //   'Cookies': 'sessionid=value; csrftoken=value;',
     // },
-        method: 'POST',
+        //method: 'POST',
         headers: {
           'x-rapidapi-key': process.env.API_KEY,
-          'x-rapidapi-host': 'instagram120.p.rapidapi.com',
+          'x-rapidapi-host': 'instagram-profile1.p.rapidapi.com',
           'Content-Type': 'application/json'
         },
         body: JSON.stringify({ username: process.env.USERNAME })
@@ -35,9 +36,9 @@ const fetchIGFollowerCount = async () => {
 
     // Extract the metrics
     const metrics = {
-        followers_count: data.result.edge_followed_by.count,//data.data.user.edge_followed_by.count,
-        following_count: data.result.edge_follow.count,//data.data.user.edge_follow.count,
-        posts_count: data.result.edge_owner_to_timeline_media.count//data.data.user.edge_owner_to_timeline_media.count
+        followers_count: data.followers,//data.result.edge_followed_by.count,//data.data.user.edge_followed_by.count,
+        following_count: data.following,//data.result.edge_follow.count,//data.data.user.edge_follow.count,
+        posts_count: data.media_count,//data.result.edge_owner_to_timeline_media.count//data.data.user.edge_owner_to_timeline_media.count
     };
 
     // Write the metrics to the environment file
