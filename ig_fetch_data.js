6f8a45feffe836dd0495c342b998e1bf5886fc21
git <contact@yoyo.dev>
2026-02-25 17:03:24 +0100

    Fix IG fetch

--- ig_fetch_data.js
+++ ig_fetch_data.js
@@ -3,19 +3,31 @@ const fs = require('fs');
 const randomUseragent = require('random-useragent');
 
 const fetchIGFollowerCount = async () => {
-  const response = await fetch('https://i.instagram.com/api/v1/users/web_profile_info/?username='+process.env.USERNAME, {
-    headers: {
-      'Accept': '*/*',
-      'Accept-Encoding': 'gzip, deflate, br',
-      'Accept-Language': 'en-US,en;q=0.9',
-      'Connection': 'keep-alive',
-      'User-Agent': "Instagram 76.0.0.15.395 Android (24/7.0; 640dpi; 1440x2560; samsung; SM-G930F; herolte; samsungexynos8890; en_US; 138226743)",
-      'Origin': 'https://www.instagram.com',
-      'Referer': 'https://www.instagram.com',
-      "X-IG-App-ID": "936619743392459",
-      'Cookies': 'sessionid=value; csrftoken=value;',
-    }
-  });
+  const response = await fetch(
+      'https://instagram120.p.rapidapi.com/api/instagram/profile'
+      //'https://i.instagram.com/api/v1/users/web_profile_info/?username='+process.env.USERNAME
+      , {
+    // headers: {
+    //   'Accept': '*/*',
+    //   'Accept-Encoding': 'gzip, deflate, br',
+    //   'Accept-Language': 'en-US,en;q=0.9',
+    //   'Connection': 'keep-alive',
+    //   'User-Agent': "Instagram 76.0.0.15.395 Android (24/7.0; 640dpi; 1440x2560; samsung; SM-G930F; herolte; samsungexynos8890; en_US; 138226743)",
+    //   'Origin': 'https://www.instagram.com',
+    //   'Referer': 'https://www.instagram.com',
+    //   "X-IG-App-ID": "936619743392459",
+    //   'Cookies': 'sessionid=value; csrftoken=value;',
+    // },
+        method: 'POST',
+        headers: {
+          'x-rapidapi-key': process.env.API_KEY,
+          'x-rapidapi-host': 'instagram120.p.rapidapi.com',
+          'Content-Type': 'application/json'
+        },
+        body: {
+          username: process.env.USERNAME
+        }
+      });
 
   if (!response.ok) {
     throw new Error(`HTTP error! status: ${response.status}`);//TODO 429 Too Many Requests
@@ -25,9 +37,9 @@ const fetchIGFollowerCount = async () => {
 
     // Extract the metrics
     const metrics = {
-        followers_count: data.data.user.edge_followed_by.count,
-        following_count: data.data.user.edge_follow.count,
-        posts_count: data.data.user.edge_owner_to_timeline_media.count
+        followers_count: data.result.edge_followed_by.count,//data.data.user.edge_followed_by.count,
+        following_count: data.result.edge_follow.count,//data.data.user.edge_follow.count,
+        posts_count: data.result.edge_owner_to_timeline_media.count//data.data.user.edge_owner_to_timeline_media.count
     };
 
     // Write the metrics to the environment file
